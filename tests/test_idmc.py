2813eef8ab9e46bd54d0416d550db8f908ad7c7b
Mike <mcarans@yahoo.co.uk>
2025-07-01 16:38:55 +1200

    HDXDSYS-2193 Make changes to IDU text as set out by IDMC (#3)
    * Update setup
    
    * Update text

--- tests/test_idmc.py
+++ tests/test_idmc.py
@@ -3,20 +3,15 @@
 Unit tests for IDMC
 
 """
+
 from os.path import join
 
-import pytest
-from hdx.api.configuration import Configuration
-from hdx.api.locations import Locations
-from hdx.data.vocabulary import Vocabulary
-from hdx.location.country import Country
+from hdx.scraper.idmc.idu.pipeline import Pipeline
 from hdx.utilities.compare import assert_files_same
 from hdx.utilities.dateparse import parse_date
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import temp_dir
 from hdx.utilities.retriever import Retrieve
-from hdx.utilities.useragent import UserAgent
-from idmc import IDMC
 
 
 class TestIDMC:
@@ -26,19 +21,41 @@ class TestIDMC:
         "groups": [{"name": "ind"}],
         "maintainer": "196196be-6037-4488-8b71-d786adf4c081",
         "name": "idmc-event-data-for-ind",
-        "notes": "Conflict and disaster population movement (flows) data for India. "
-        "The data is the most recent available and covers a 180 day time "
-        "period.\n"
+        "notes": "Conflict and disaster population movement (flows) data for India. \n"
         "\n"
-        "Internally displaced persons are defined according to [the 1998 "
-        "Guiding Principles]"
-        "(https://www.internal-displacement.org/internal-displacement/guiding-principles-on-internal-displacement) "
+        "The **IDU (Internal Displacement Updates) dataset**, provided by "
+        "the [Internal Displacement Monitoring Centre "
+        "(IDMC)](https://www.internal-displacement.org/), offers timely "
+        "event data and provisional information on new internal "
+        "displacements caused by conflicts and disasters. Representing the "
+        "most recent available information over a 180-day time period, the "
+        'IDU is updated daily and focuses on "flows" (new displacements).\n'
+        "\n"
+        "Internally displaced persons (IDPs) are defined according to the "
+        "[1998 Guiding "
+        "Principles](https://www.internal-displacement.org/internal-displacement/guiding-principles-on-internal-displacement/) "
         "as people or groups of people who have been forced or obliged to "
         "flee or to leave their homes or places of habitual residence, in "
         "particular as a result of armed conflict, or to avoid the effects "
         "of armed conflict, situations of generalized violence, violations "
         "of human rights, or natural or human-made disasters and who have "
-        "not crossed an international border.\n"
+        "not crossed an international border. The IDMC's event data, sourced "
+        "from the IDU, provides initial assessments of these internal "
+        "displacements, reflecting continually updated provisional "
+        "information from various sources.\n"
+        "\n"
+        "While the IDU offers early insights, the more thoroughly validated "
+        'and curated "stock"  (Total number of people leaving on internal '
+        'displacement) and "flow" (population movements) estimates are '
+        "available in the annual [Global Internal Displacement Database "
+        "(GIDD)](http://www.internal-displacement.org/database/displacement-data). "
+        "Both datasets are accessible via API, with specific guidance on "
+        "data access, structure, and limitations, including important "
+        "preprocessing considerations for the IDU to ensure accurate "
+        "analysis and avoid double-counting. For further detailed "
+        "information and complete API specifications, users are encouraged "
+        "to consult the official documentation at "
+        "https://www.internal-displacement.org/database/api-documentation/.\n"
         "\n"
         "The IDMC's Event data, sourced from the Internal Displacement "
         "Updates (IDU), offers initial assessments of internal displacements "
@@ -47,10 +64,10 @@ class TestIDMC:
         "basis, reflecting the availability of data on new displacements "
         "arising from conflicts and disasters. The finalized, carefully "
         "curated, and validated estimates are then made accessible through "
-        "[the Global Internal Displacement Database (GIDD)]"
-        "(https://www.internal-displacement.org/database/displacement-data). The "
-        "IDU dataset comprises preliminary estimates aggregated from various "
-        "publishers or sources.\n",
+        "[the Global Internal Displacement Database "
+        "(GIDD)](https://www.internal-displacement.org/database/displacement-data). "
+        "The IDU dataset comprises preliminary estimates aggregated from "
+        "various publishers or sources.\n",
         "owner_org": "647d9d8c-4cac-4c33-b639-649aad1c2893",
         "subnational": "0",
         "tags": [
@@ -92,8 +109,7 @@ class TestIDMC:
     ind_showcase = {
         "image_url": "https://www.internal-displacement.org/sites/default/files/logo_0.png",
         "name": "idmc-event-data-for-ind-showcase",
-        "notes": "Click the image to go to the IDMC summary page for the India "
-        "dataset",
+        "notes": "Click the image to go to the IDMC summary page for the India dataset",
         "tags": [
             {
                 "name": "conflict-violence",
@@ -135,15 +151,39 @@ class TestIDMC:
         "movement (flows) data recorded for Afghanistan in the last 180 "
         "days.\n"
         "\n"
-        "Internally displaced persons are defined according to [the 1998 "
-        "Guiding Principles]"
-        "(https://www.internal-displacement.org/internal-displacement/guiding-principles-on-internal-displacement) "
+        "The **IDU (Internal Displacement Updates) dataset**, provided by "
+        "the [Internal Displacement Monitoring Centre "
+        "(IDMC)](https://www.internal-displacement.org/), offers timely "
+        "event data and provisional information on new internal "
+        "displacements caused by conflicts and disasters. Representing the "
+        "most recent available information over a 180-day time period, the "
+        'IDU is updated daily and focuses on "flows" (new displacements).\n'
+        "\n"
+        "Internally displaced persons (IDPs) are defined according to the "
+        "[1998 Guiding "
+        "Principles](https://www.internal-displacement.org/internal-displacement/guiding-principles-on-internal-displacement/) "
         "as people or groups of people who have been forced or obliged to "
         "flee or to leave their homes or places of habitual residence, in "
         "particular as a result of armed conflict, or to avoid the effects "
         "of armed conflict, situations of generalized violence, violations "
         "of human rights, or natural or human-made disasters and who have "
-        "not crossed an international border.\n"
+        "not crossed an international border. The IDMC's event data, sourced "
+        "from the IDU, provides initial assessments of these internal "
+        "displacements, reflecting continually updated provisional "
+        "information from various sources.\n"
+        "\n"
+        "While the IDU offers early insights, the more thoroughly validated "
+        'and curated "stock"  (Total number of people leaving on internal '
+        'displacement) and "flow" (population movements) estimates are '
+        "available in the annual [Global Internal Displacement Database "
+        "(GIDD)](http://www.internal-displacement.org/database/displacement-data). "
+        "Both datasets are accessible via API, with specific guidance on "
+        "data access, structure, and limitations, including important "
+        "preprocessing considerations for the IDU to ensure accurate "
+        "analysis and avoid double-counting. For further detailed "
+        "information and complete API specifications, users are encouraged "
+        "to consult the official documentation at "
+        "https://www.internal-displacement.org/database/api-documentation/.\n"
         "\n"
         "The IDMC's Event data, sourced from the Internal Displacement "
         "Updates (IDU), offers initial assessments of internal displacements "
@@ -152,8 +192,8 @@ class TestIDMC:
         "basis, reflecting the availability of data on new displacements "
         "arising from conflicts and disasters. The finalized, carefully "
         "curated, and validated estimates are then made accessible through "
-        "[the Global Internal Displacement Database (GIDD)]"
-        "(https://www.internal-displacement.org/database/displacement-data). "
+        "[the Global Internal Displacement Database "
+        "(GIDD)](https://www.internal-displacement.org/database/displacement-data). "
         "The IDU dataset comprises preliminary estimates aggregated from "
         "various publishers or sources.\n",
         "owner_org": "647d9d8c-4cac-4c33-b639-649aad1c2893",
@@ -180,56 +220,6 @@ class TestIDMC:
         "url_type": "upload",
     }
 
-    @pytest.fixture(scope="function")
-    def fixtures(self):
-        return join("tests", "fixtures")
-
-    @pytest.fixture(scope="function")
-    def configuration(self):
-        Configuration._create(
-            hdx_read_only=True,
-            user_agent="test",
-            project_config_yaml=join("config", "project_configuration.yaml"),
-        )
-        UserAgent.set_global("test")
-        Locations.set_validlocations(
-            [{"name": "afg", "title": "Afghanistan"}, {"name": "ind", "title": "India"}]
-        )
-        Country.countriesdata(use_live=False)
-        tags = (
-            "conflict-violence",
-            "cyclones-hurricanes-typhoons",
-            "displacement",
-            "flooding-storm surge",
-            "hxl",
-            "internally displaced persons-idp",
-            "natural disasters",
-        )
-        Vocabulary._tags_dict = {tag: {"Action to Take": "ok"} for tag in tags}
-        Vocabulary._tags_dict["conflict"] = {
-            "Action to Take": "merge",
-            "New Tag(s)": "conflict-violence",
-        }
-        Vocabulary._tags_dict["cyclone"] = {
-            "Action to Take": "merge",
-            "New Tag(s)": "cyclones-hurricanes-typhoons",
-        }
-        Vocabulary._tags_dict["erosion"] = {
-            "Action to Take": "merge",
-            "New Tag(s)": "natural disasters",
-        }
-        Vocabulary._tags_dict["flood"] = {
-            "Action to Take": "merge",
-            "New Tag(s)": "flooding-storm surge",
-        }
-        tags = [{"name": tag} for tag in tags]
-        Vocabulary._approved_vocabulary = {
-            "tags": tags,
-            "id": "4e61d464-4943-4e97-973a-84673c1aaa87",
-            "name": "approved",
-        }
-        return Configuration.read()
-
     def test_generate_dataset_and_showcase(self, configuration, fixtures):
         with temp_dir(
             "test_idmc", delete_on_success=True, delete_on_failure=False
@@ -238,21 +228,21 @@ class TestIDMC:
                 retriever = Retrieve(downloader, folder, fixtures, folder, False, True)
                 # indicator dataset test
                 today = parse_date("2023-11-14")
-                idmc = IDMC(configuration, retriever, today, folder)
-                idmc.get_idmc_territories()
-                countries = idmc.get_countriesdata()
+                pipeline = Pipeline(configuration, retriever, today, folder)
+                pipeline.get_idmc_territories()
+                countries = pipeline.get_countriesdata()
                 assert len(countries) == 167
 
                 (
                     dataset,
                     showcase,
                     show_quickcharts,
-                ) = idmc.generate_dataset_and_showcase("IND")
+                ) = pipeline.generate_dataset_and_showcase("IND")
                 assert dataset == self.ind_dataset
                 resources = dataset.get_resources()
                 assert resources[0] == self.ind_resource
                 file = "event_data_IND.csv"
-                assert_files_same(join("tests", "fixtures", file), join(folder, file))
+                assert_files_same(join(fixtures, file), join(folder, file))
                 assert showcase == self.ind_showcase
                 assert show_quickcharts is True
 
@@ -260,11 +250,11 @@ class TestIDMC:
                     dataset,
                     showcase,
                     show_quickcharts,
-                ) = idmc.generate_dataset_and_showcase("AFG")
+                ) = pipeline.generate_dataset_and_showcase("AFG")
                 assert dataset == self.afg_dataset
                 resources = dataset.get_resources()
                 assert resources[0] == self.afg_resource
                 file = "event_data_AFG.csv"
-                assert_files_same(join("tests", "fixtures", file), join(folder, file))
+                assert_files_same(join(fixtures, file), join(folder, file))
                 assert showcase is None
                 assert show_quickcharts is False
